892ee7b75286fe2701072c2d2b0e010fe3099540
Konozo24 <mingwei12242016@gmail.com>
2026-03-20 16:46:50 +0800

    First commit

---
+++ scrapeLogic.js
@@ -0,0 +1,42 @@
+const puppeteer = require("puppeteer");
+
+const scrapeLogic = async (res) => {
+    const browser = await puppeteer.launch();
+    try {
+        // Launch the browser and open a new blank page.
+        const page = await browser.newPage();
+
+        // Navigate the page to a URL.
+        await page.goto('https://developer.chrome.com/');
+
+        // Set screen size.
+        await page.setViewport({ width: 1080, height: 1024 });
+
+        // Open the search menu using the keyboard.
+        await page.keyboard.press('/');
+
+        // Type into search box using accessible input name.
+        await page.locator('::-p-aria(Search)').fill('automate beyond recorder');
+
+        // Wait and click on first result.
+        await page.locator('.devsite-result-item-link').click();
+
+        // Locate the full title with a unique string.
+        const textSelector = await page
+            .locator('::-p-text(Customize and automate)')
+            .waitHandle();
+        const fullTitle = await textSelector?.evaluate(el => el.textContent);
+
+        // Print the full title.
+        const logStatement = `The title of this blog post is ${fullTitle}`;
+        console.log(logStatement);
+        res.send(logStatement);
+    } catch (e) {
+        console.error(e);
+        res.send("Something went wrong when running Puppeteer");
+    } finally {
+        if (browser) await browser.close();
+    }
+}
+
+module.exports = { scrapeLogic };
